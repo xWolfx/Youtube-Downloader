460b01c2f3fca78094084055d13f3ac623f067c9
Henri Herrera <47834654+xWolfx@users.noreply.github.com>
2019-11-25 06:52:53 -0400

    Editing shell script

--- Youtube.sh
+++ Youtube.sh
@@ -2,15 +2,11 @@ echo 'Hello! Starting now...'
 echo 'Make sure you downloaded into the Downloads/ folder'
 sleep 3
 echo 'Now, can you please enter your password?'
-sudo apt install curl
 sudo apt install python3
 sudo apt install youtube-dl
 sudo apt install ffmpeg
-cd $HOME/Downloads/
-mv 'YouTube Downloader Linux'/'YouTube Downloader.desktop' $HOME/Desktop/
-mv 'YouTube Downloader Linux' $HOME/
-cd $HOME/Desktop/
-chmod +x 'YouTube Downloader.desktop'
-echo 'Nice! Now you can just double-click the launcher called Youtube Downloader in your desktop'
-rm Launcher.sh
+echo 'If everything was installed successfully...
+echo 'You can now paste or write when the script is inside your home folder:'
+echo 'python3 Youtube-Script.py'
+sleep 3
 exit
