39b678f01584166ad2b284a2030125b379fc0f63
Henri Herrera <47834654+xWolfx@users.noreply.github.com>
2019-12-01 10:27:04 -0400

    Making the echo outputs more understandable

--- Youtube.sh
+++ Youtube.sh
@@ -1,12 +1,12 @@
 echo 'Hello! Starting now...'
-echo 'Make sure you downloaded into the Downloads/ folder'
+echo 'Make sure you downloaded into the Downloads folder'
 sleep 3
 echo 'Now, can you please enter your password?'
 sudo apt install python3
 sudo apt install youtube-dl
 sudo apt install ffmpeg
 echo 'If everything was installed successfully...
-echo 'You can now paste or write when the script is inside your home folder:'
+echo 'You can now paste or write inside the terminal when the script is inside your home folder:'
 echo 'python3 Youtube-Script.py'
 sleep 3
 exit
